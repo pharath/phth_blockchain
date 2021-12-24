824446ed120d31b055799abe4eb591c3ac4b5bf3
pharath <phrth2@gmail.com>
2021-12-24 12:40:33 +0100

    modification by phth

---
+++ scripts/push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash                                                                
+git status                                                                 
+git add --all                                                              
+git commit -m "modification by phth"                                       
+git push origin master                                                     
+git status
