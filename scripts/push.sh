f939b6570dc6544c5c3723d17e88397cb744fb33
pharath <phrth2@gmail.com>
2021-12-24 18:13:45 +0100

    new push script

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,6 +1,12 @@
 #!/bin/bash                                                                
+printf "================ OLD git status =================\n"
 git status                                                                 
+printf "=================== git add =====================\n"
 git add --all                                                              
-git commit -m "modification by phth"                                       
+printf "================== git commit ===================\n"
+read -p "Commit description: " desc
+git commit -m "$desc"
+printf "============== push origin master ===============\n"
 git push origin master                                                     
+printf "================ NEW git status =================\n"
 git status
